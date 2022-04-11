a83e346566d28c38d21d7eb2d1c8494265216b49
Jerusheya <jerusheya2002@gmail.com>
2022-04-11 16:34:10 +0530

    added java practice

---
+++ Java/operators.java
@@ -0,0 +1,26 @@
+
+
+public class operators {
+
+    public static void  main( String [] args){
+        
+        int a = 12, b = 5;
+
+        // addition:
+        System.out.println("a + b = " + (a + b));
+    
+        // subtraction:
+        System.out.println("a - b = " + (a - b));
+    
+        // multiplication:
+        System.out.println("a * b = " + (a * b));
+    
+        // division:
+        System.out.println("a / b = " + (a / b));
+    
+        // modulo:
+        System.out.println("a % b = " + (a % b));
+      }
+    
+    
+}
---
+++ Java/typecasting.java
@@ -0,0 +1,26 @@
+public class typecasting {
+    public static void main(String[] args) {
+
+        //widening casting:
+
+        //byte -> short -> char -> int -> long -> float -> double
+
+        // widening casting: int to double
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+    
+        System.out.println(myInt);  
+        System.out.println(myDouble);  
+
+        //narrowing casting:
+
+        //double -> float -> long -> int -> char -> short -> byte
+
+        //narrowing casting:
+        double mydouble = 9.78d;
+        int myint = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(mydouble);   
+        System.out.println(myint); 
+      }
+}
