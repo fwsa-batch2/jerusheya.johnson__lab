98771c497a39f3bf20df8eb8542bd0145412304d
Jerusheya <jerusheya2002@gmail.com>
2022-06-22 14:45:07 +0530

    changed the lookup.rb file

--- Ruby/WD201/DNS resolver/lookup.rb
+++ Ruby/WD201/DNS resolver/lookup.rb
@@ -14,30 +14,34 @@ dns_raw = File.readlines("zone") #stores as an array of array
 
 def parse_dns(dns_raw)
 
-
- dns_record = {}
-      for i in (dns_raw.select{ |x| x[0] != "#" && x!="\n"})
-        @dns_domain=(i.split(","))[1].strip()
-        @dns_address=(i.split(","))[2].strip()
-        dns_record[@dns_domain]=@dns_address
-      end
-      p dns_record
-      return dns_record
+  dns_raw.
+  reject { |line| line[0] == "#" } #deletes the line starts with #
+  .map { |line| line.split(",") } #splits the line with , and makes it as a array
+  .reject do |record|
+    record.length != 3  #deletes the line if the line contains 3 words (for expelling the empty line)
+  end
+  .each_with_object({}) do |record, records|
+    rec_index_value = record.map{|rec|  rec.strip()} #strips each value in the array
+    #records = hash, record = final array with no empty lines , rec_index_value = array without \n 
+    records[rec_index_value[1]] = { type: rec_index_value[0], target: rec_index_value[2] }
 end
 
-def resolve(dns_record, lookup_chain, domain)
-  
-     if (dns_record.has_key? domain)
-        dns_domain2= dns_record[domain]
-        lookup_chain.push(dns_record[domain])
-         if (dns_record.has_key? dns_domain2)
-           resolve(dns_record,lookup_chain,dns_domain2)
-         end
-     else
-        lookup_chain.push("this is not present")
-     end
-     return lookup_chain
 
+end
+
+def resolve(dns_records, lookup_chain, domain)
+  record = dns_records[domain]
+  if (!record)
+    lookup_chain << "Error: Record not found for " + domain
+  elsif record[:type] == "A"
+    lookup_chain << record[:target]
+  elsif record[:type] == "CNAME"
+    lookup_chain << record[:target]
+    resolve(dns_records, lookup_chain, record[:target])
+  else
+    lookup_chain << "Invalid record type for " + domain
+  end
+  return lookup_chain
 end
 
 dns_record = parse_dns(dns_raw)
